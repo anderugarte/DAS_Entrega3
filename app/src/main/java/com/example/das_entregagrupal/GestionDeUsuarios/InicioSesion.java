dc8d9b84fa92d6714a5bb80be49db54c05feb0d8
ander <anderugarte1999@gmail.com>
2021-04-30 21:21:40 +0200

    Modificaciones interfaz Inicio Sesión

--- app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
+++ app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
@@ -2,7 +2,10 @@ package com.example.das_entregagrupal.GestionDeUsuarios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.das_entregagrupal.R;
 
@@ -12,5 +15,17 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
+        Button bNTCA = (Button) findViewById(R.id.bNTCA);
+
+        bNTCA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent r = new Intent (getBaseContext(), Registro.class);
+                startActivity(r);
+                finish();
+            }
+        });
+
     }
 }
