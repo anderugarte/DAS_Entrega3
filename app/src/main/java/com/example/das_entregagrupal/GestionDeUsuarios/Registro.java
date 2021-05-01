a83ba09660179ae26ef6e1cf04129c99a0d6c5ad
ander <anderugarte1999@gmail.com>
2021-05-01 17:58:07 +0200

    Interfaz de Registro editada. Interfaz de Registro land creada y editada. Modificado el color de la fuente del estilo.

--- app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/Registro.java
+++ app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/Registro.java
@@ -2,8 +2,13 @@ package com.example.das_entregagrupal.GestionDeUsuarios;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.das_entregagrupal.Principal.ClaseDialogoFecha;
 import com.example.das_entregagrupal.R;
 
 public class Registro extends AppCompatActivity {
@@ -12,5 +17,37 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        EditText etCumple = (EditText) findViewById(R.id.etCumpleanos);
+        Button bInicSes = (Button) findViewById(R.id.bIniciarSesion);
+
+        // Al pulsar este EditText, desplegaremos un dialogo donde se podra seleccionar la fecha de nacimientop del usuario
+        etCumple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.etCumpleanos:
+                        showDatePickerDialog(etCumple);
+                        break;
+                }
+            }
+        });
+
+        bInicSes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent is = new Intent (getBaseContext(), InicioSesion.class);
+                startActivity(is);
+                finish();
+            }
+        });
     }
+
+    // Este metodo nos ayuda a desplegar el dialogo para la seleccion de la fecha de nacimiento y nos permite enviar
+    // el EditText por parametro para una vez obtenida la fecha poder realizar un .setText("fecha") en el
+    private void showDatePickerDialog(final EditText editText) {
+        ClaseDialogoFecha dialogoCumpleanos = new ClaseDialogoFecha(editText);
+        dialogoCumpleanos.show(getSupportFragmentManager(),"cumple");
+    }
+
 }
