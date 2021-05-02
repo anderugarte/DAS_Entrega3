258796a4657493e75260bbb384c1b15f6731b21b
ander <anderugarte1999@gmail.com>
2021-05-02 22:05:22 +0200

    Creada la interfaz de Ayuda y Ayuda land.

--- app/src/main/java/com/example/das_entregagrupal/Principal/Opciones.java
+++ app/src/main/java/com/example/das_entregagrupal/Principal/Opciones.java
@@ -2,7 +2,10 @@ package com.example.das_entregagrupal.Principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.das_entregagrupal.R;
 
@@ -12,5 +15,32 @@ public class Opciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opciones);
+
+        Button bMiPerfil = (Button) findViewById(R.id.bPerfil);
+        Button bMMPunt = (Button) findViewById(R.id.bMMPuntuciaones);
+        Button bAyuda = (Button) findViewById(R.id.bAyuda);
+
+        bMiPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bMMPunt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bAyuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent (getBaseContext(), Ayuda.class);
+                startActivity(a);
+            }
+        });
+
     }
 }
