c4200e29738242dce0f1ddbf05bc4eacb2749550
ander <anderugarte1999@gmail.com>
2021-05-02 21:51:37 +0200

    Creada la interfaz de Opciones y Opciones land.

--- app/src/main/java/com/example/das_entregagrupal/Principal/MenuPrincipal.java
+++ app/src/main/java/com/example/das_entregagrupal/Principal/MenuPrincipal.java
@@ -2,7 +2,10 @@ package com.example.das_entregagrupal.Principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.das_entregagrupal.R;
 
@@ -12,5 +15,40 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        Button bJContraIA = (Button) findViewById(R.id.bJIA);
+        Button bJ2Jugadores = (Button) findViewById(R.id.bJ2J);
+        Button bJOnline = (Button) findViewById(R.id.bJO);
+        Button bOpciones = (Button) findViewById(R.id.bO);
+
+        bJContraIA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bJ2Jugadores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bJOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        bOpciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent o = new Intent (getBaseContext(), Opciones.class);
+                startActivity(o);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/das_entregagrupal/Principal/Opciones.java
@@ -0,0 +1,16 @@
+package com.example.das_entregagrupal.Principal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.das_entregagrupal.R;
+
+public class Opciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opciones);
+    }
+}
