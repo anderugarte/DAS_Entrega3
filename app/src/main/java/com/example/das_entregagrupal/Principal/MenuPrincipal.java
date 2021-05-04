e36026a4fc4d08506746f2785aa28f011068df40
ander <anderugarte1999@gmail.com>
2021-05-04 11:24:15 +0200

    onBackPressed() generado en el Menu Principal.

--- app/src/main/java/com/example/das_entregagrupal/Principal/MenuPrincipal.java
+++ app/src/main/java/com/example/das_entregagrupal/Principal/MenuPrincipal.java
@@ -1,7 +1,9 @@
 package com.example.das_entregagrupal.Principal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +14,15 @@ import com.example.das_entregagrupal.R;
 
 public class MenuPrincipal extends AppCompatActivity {
 
+    private AlertDialog.Builder alertDialogBuilder;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        context = this;
 
         Button bJContraIA = (Button) findViewById(R.id.bJIA);
         Button bJ2Jugadores = (Button) findViewById(R.id.bJ2J);
@@ -52,4 +59,21 @@ public class MenuPrincipal extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public void onBackPressed(){
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Bichötes: Conecta 4");
+        alertDialogBuilder.setMessage("¿Desea cerrar la sesión?")
+                .setCancelable(false)
+                .setPositiveButton("Aceptar", (dialog, which) -> {
+                    // Cerrar sesión
+                    Intent ma = new Intent(context, MainActivity.class);
+                    ma.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(ma);
+                    finish();
+                })
+                .setNegativeButton("Cancelar", (dialog, which) -> dialog.cancel()).create().show();
+    }
+
 }
