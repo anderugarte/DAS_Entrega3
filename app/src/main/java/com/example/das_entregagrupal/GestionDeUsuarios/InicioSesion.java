8e695e8f27f3498880bb0d66051211a254471f22
iratxe <iiratxe.ggonzalez@gmail.com>
2021-05-09 18:49:10 +0200

    Login implementado pero hash_verify siempre da resultado false.

--- app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
+++ app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
@@ -10,6 +10,8 @@ import androidx.work.WorkManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.AutoText;
+import android.text.Editable;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -17,21 +19,20 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.das_entregagrupal.BaseDeDatos.ConexionLogin;
+import com.example.das_entregagrupal.BaseDeDatos.ConexionRegistro;
 import com.example.das_entregagrupal.Principal.MenuPrincipal;
 import com.example.das_entregagrupal.R;
 
 public class InicioSesion extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
-
-        EditText etNU = (EditText) findViewById(R.id.etNU);
-        EditText etC = (EditText) findViewById(R.id.etC);
-
         Button bNTCA = (Button) findViewById(R.id.bNTCA);
-
         bNTCA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,82 +43,56 @@ public class InicioSesion extends AppCompatActivity {
         });
 
         Button bIniciarSesion = (Button) findViewById(R.id.bIniciarSesion);
+        username = (EditText) findViewById(R.id.etNU);
+        password = (EditText) findViewById(R.id.etC);
 
-        // Se realizan las comprobaciones para el inicio de sesion
         bIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                // Se recogen los campos rellenados por el usuario
-                String user = etNU.getText().toString();
-                String pass = etC.getText().toString();
-
-                // Gestion del inicio de sesion
-                // Primero, se comprueba si existen campos vacios
-                if (user.length()==0 || pass.length()==0) {
-                    if (user.length()==0) { // Campo username vacio
-                        int tiempo= Toast.LENGTH_SHORT;
-                        Toast aviso = Toast.makeText(getApplicationContext(), "Nombre de usuario vacío", tiempo);
-                        aviso.setGravity(Gravity.BOTTOM| Gravity.CENTER, 0, 0);
-                        aviso.show();
-                    } else { // Campo contrasena vacia
-                        int tiempo= Toast.LENGTH_SHORT;
-                        Toast aviso = Toast.makeText(getApplicationContext(), "Contraseña vacía", tiempo);
-                        aviso.setGravity(Gravity.BOTTOM| Gravity.CENTER, 0, 0);
-                        aviso.show();
-                    }
-                } else if (user.length()>0 && pass.length()>0) { // No existe campos vacios, se comprobara si los campos introducidos por el usuario son correctos
-
-                    ////////////////////// TEMPORAL ///////////////////////////////
-                    Intent mp = new Intent (getBaseContext(), MenuPrincipal.class);
-                    mp.putExtra("username",etNU.getText().toString());
-                    startActivity(mp);
-                    finish();
-                    ///////////////////////////////////////////////////////////////
-
-                    // Se accede al metodo que gestiona la conexion e inicio de sesion
-                    //gestionarConexion(user,pass);
+                Intent mp = new Intent (getBaseContext(), MenuPrincipal.class);
+                startActivity(mp);
+                finish();
+                if (username.getText().toString().equals("") ||
+                        password.getText().toString().equals("")) {
+                    String text = "Rellena todos los campos";
+                    Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
+                    toast.show();
+                } else {
+                    iniciarSesion();
                 }
-
             }
         });
 
     }
 
-    // Metodo que gestiona el inicio de sesion del usuario
-    private void gestionarConexion(String user, String pass) {
-
-        Data resultados = new Data.Builder()
-                .putString("username",user)
-                .putString("password",pass)
-                .build();
-
-        OneTimeWorkRequest trabajoPuntualIS = new OneTimeWorkRequest.Builder(ConexionLogin.class)
-                .setInputData(resultados)
-                .build();
-
-        WorkManager.getInstance(getApplicationContext()).getWorkInfoByIdLiveData(trabajoPuntualIS.getId())
+    private void iniciarSesion() {
+        Data datos = new Data.Builder().putString("username", username.getText().toString())
+                .putString("password", password.getText().toString()).build();
+        OneTimeWorkRequest otwr = new OneTimeWorkRequest.Builder(ConexionLogin.class)
+                .setInputData(datos).build();
+        WorkManager.getInstance(getBaseContext()).getWorkInfoByIdLiveData(otwr.getId())
                 .observe(this, new Observer<WorkInfo>() {
                     @Override
-                    public void onChanged(WorkInfo status) {
-                        if (status != null && status.getState().isFinished()) {
-                            if (status.getOutputData().getString("result").equals("true")) {
-                                // Login correcto (existe un usuario con dichas credenciales asi que se inicia sesion)
-                                Intent mp = new Intent (getBaseContext(), MenuPrincipal.class);
-                                mp.putExtra("username", user);
+                    public void onChanged(WorkInfo workInfo) {
+                        if (workInfo != null && workInfo.getState().isFinished()) {
+                            Log.i("hola", workInfo.getOutputData().getString("result"));
+                            if (workInfo.getOutputData().getString("result").equals("logOK")) {
+                                // El inicio de sesion se ha realizado correctamente
+                                // Intent MenuPrincipal
+                                Intent mp = new Intent(getBaseContext(), MenuPrincipal.class);
+                                mp.putExtra("username", username.getText().toString());
                                 startActivity(mp);
                                 finish();
                             } else {
-                                // Login incorrecto (no existe ningun usuario con dichas credenciales)
-                                int tiempo= Toast.LENGTH_SHORT;
-                                Toast aviso = Toast.makeText(getApplicationContext(), "Error / Campos incorrectos", tiempo);
-                                aviso.setGravity(Gravity.BOTTOM| Gravity.CENTER, 0, 0);
-                                aviso.show();
+                                String text = "Usuario o contraseña incorrectos";
+                                Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
+                                toast.show();
                             }
                         }
                     }
                 });
-        WorkManager.getInstance(getApplicationContext()).enqueue(trabajoPuntualIS);
-
+        WorkManager.getInstance(getBaseContext()).enqueue(otwr);
     }
 }
