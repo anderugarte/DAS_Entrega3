752a90b39fac74f7b59b92355a48d17c0349e591
iratxe <iiratxe.ggonzalez@gmail.com>
2021-05-09 18:56:16 +0200

    Pequeños cambios en InicioSesion

--- app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
+++ app/src/main/java/com/example/das_entregagrupal/GestionDeUsuarios/InicioSesion.java
@@ -32,6 +32,7 @@ public class InicioSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+
         Button bNTCA = (Button) findViewById(R.id.bNTCA);
         bNTCA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,50 +50,57 @@ public class InicioSesion extends AppCompatActivity {
         bIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent mp = new Intent (getBaseContext(), MenuPrincipal.class);
-                startActivity(mp);
-                finish();
-                if (username.getText().toString().equals("") ||
-                        password.getText().toString().equals("")) {
-                    String text = "Rellena todos los campos";
+                if (username.getText().toString().equals("")) {
+                    String text = "Nombre de usuario vacío";
+                    Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
+                    toast.show();
+                } else if (password.getText().toString().equals("")) {
+                    String text = "Contraseña vacía";
                     Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
                     toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
                     toast.show();
                 } else {
-                    iniciarSesion();
+                    // HASTA ARREGLAR INICIO DE SESIÓN
+                    Intent mp = new Intent (getBaseContext(), MenuPrincipal.class);
+                    mp.putExtra("username",username.getText().toString());
+                    startActivity(mp);
+                    finish();
+                    // INICIAR SESIÓN
+                    //iniciarSesion();
                 }
             }
         });
 
     }
 
-    private void iniciarSesion() {
-        Data datos = new Data.Builder().putString("username", username.getText().toString())
-                .putString("password", password.getText().toString()).build();
-        OneTimeWorkRequest otwr = new OneTimeWorkRequest.Builder(ConexionLogin.class)
-                .setInputData(datos).build();
-        WorkManager.getInstance(getBaseContext()).getWorkInfoByIdLiveData(otwr.getId())
-                .observe(this, new Observer<WorkInfo>() {
-                    @Override
-                    public void onChanged(WorkInfo workInfo) {
-                        if (workInfo != null && workInfo.getState().isFinished()) {
-                            Log.i("hola", workInfo.getOutputData().getString("result"));
-                            if (workInfo.getOutputData().getString("result").equals("logOK")) {
-                                // El inicio de sesion se ha realizado correctamente
-                                // Intent MenuPrincipal
-                                Intent mp = new Intent(getBaseContext(), MenuPrincipal.class);
-                                mp.putExtra("username", username.getText().toString());
-                                startActivity(mp);
-                                finish();
-                            } else {
-                                String text = "Usuario o contraseña incorrectos";
-                                Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
-                                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
-                                toast.show();
-                            }
-                        }
-                    }
-                });
-        WorkManager.getInstance(getBaseContext()).enqueue(otwr);
-    }
+//    private void iniciarSesion() {
+//        Data datos = new Data.Builder().putString("username", username.getText().toString())
+//                .putString("password", password.getText().toString()).build();
+//        OneTimeWorkRequest otwr = new OneTimeWorkRequest.Builder(ConexionLogin.class)
+//                .setInputData(datos).build();
+//        WorkManager.getInstance(getBaseContext()).getWorkInfoByIdLiveData(otwr.getId())
+//                .observe(this, new Observer<WorkInfo>() {
+//                    @Override
+//                    public void onChanged(WorkInfo workInfo) {
+//                        if (workInfo != null && workInfo.getState().isFinished()) {
+//                            Log.i("hola", workInfo.getOutputData().getString("result"));
+//                            if (workInfo.getOutputData().getString("result").equals("logOK")) {
+//                                // El inicio de sesion se ha realizado correctamente
+//                                // Intent MenuPrincipal
+//                                Intent mp = new Intent(getBaseContext(), MenuPrincipal.class);
+//                                mp.putExtra("username", username.getText().toString());
+//                                startActivity(mp);
+//                                finish();
+//                            } else {
+//                                String text = "Usuario o contraseña incorrectos";
+//                                Toast toast = Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG);
+//                                toast.setGravity(Gravity.BOTTOM | Gravity.CENTER, 0, 0);
+//                                toast.show();
+//                            }
+//                        }
+//                    }
+//                });
+//        WorkManager.getInstance(getBaseContext()).enqueue(otwr);
+//    }
 }
