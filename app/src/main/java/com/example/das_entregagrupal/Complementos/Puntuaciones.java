d2d93218da8fa1dda34c9d5d44327570958da1fa
ander <anderugarte1999@gmail.com>
2021-05-18 17:54:23 +0200

    Creadoa la interfaz de Puntuaciones.

---
+++ app/src/main/java/com/example/das_entregagrupal/Complementos/Puntuaciones.java
@@ -0,0 +1,16 @@
+package com.example.das_entregagrupal.Complementos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.das_entregagrupal.R;
+
+public class Puntuaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntuaciones);
+    }
+}
